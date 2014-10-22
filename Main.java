fd6ea9ab1579c7c111d7c4ba072a7c28f9ca50a8
NapoleonBonerfarte <niklas.zwingenberger@gmail.com>
2014-10-22 19:52:06 +0100

    Added Main.java for testing

---
+++ Main.java
@@ -0,0 +1,41 @@
+
+
+public class Main
+{
+	//Use this class to test your stuff. Just comment out or delete your code before you commit it.
+	public static void main(String[] args)
+	{
+
+	}
+}
+
+
+
+		/* Test of Board.clone() method
+
+		import java.util.Arrays;
+
+		Board a = new Board(2,2);
+		a.set(0,0,0);
+		a.set(0,1,1);
+		a.set(1,0,2);
+		a.set(1,1,3);
+
+		Board b = a.clone();
+		b.set(0,0,3);
+		b.set(0,1,2);
+		b.set(1,0,1);
+		b.set(1,1,0);
+
+		System.out.println("A:");
+		System.out.println(a.get(0,0));
+		System.out.println(a.get(0,1));
+		System.out.println(a.get(1,0));
+		System.out.println(a.get(1,1));
+
+		System.out.println("B:");
+		System.out.println(b.get(0,0));
+		System.out.println(b.get(0,1));
+		System.out.println(b.get(1,0));
+		System.out.println(b.get(1,1));
+		*/
