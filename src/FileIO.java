ece707a79523d8996b4eb1e12dcfd887120ac299
NapoleonBonerfarte <niklas.zwingenberger@gmail.com>
2014-10-22 23:35:24 +0100

    Moved to src folder. Useful for build later

--- src/FileIO.java
+++ src/FileIO.java
@@ -37,6 +37,7 @@ public class FileIO
 
 	}
 
+    //General read method - returns ArrayList of lines
     public ArrayList<String> readFile(String path)
     {
         BufferedReader reader = null;
@@ -71,41 +72,66 @@ public class FileIO
         return rawInput;
     }
     
-    public void writeFile(String path, ArrayList <String> output)
+    //General write method
+    public void writeFile(String path, String output)
     {
         BufferedWriter writer = null;
+        String p = path;
         
         try
         {
             File file = new File(path);
             
             if(!file.exists())
+            {
                 file.createNewFile();
+                System.out.println("--Writing file to "+p+" --");
+            }
             else
             {
                 System.err.println("WARNING: file already exists.");
+                p = adjustPath(path);
+                System.out.println("Writing to "+p+" instead.");
             }
-            
-            System.out.println("--Writing file to "+file+" --");
-            
+                      
             writer = new BufferedWriter(new FileWriter(file));
-            
-            for(int i = 0; i < output.length; i++)
-            {   
-                String temp = "Case #"+Integer.toString(i+1)+": "+output[i];
-                writer.write(temp);
-                writer.newLine();
-            }
+            writer.write(output);
             writer.close();                        
         }
         catch(IOException ex)
         {
-            ex.printStackTrace();
+            System.err.println("The program could not write the file to "+p)
         }
         finally
         {
             try{writer.close();}
-            catch(IOException ex){ex.printStackTrace();}
+            catch(IOException ex)
+            {
+                System.err.println("There was an error closing the writing stream.")
+            }
+        }
+    }
+
+    //Fancy method that adds a number to a path.
+    public String adjustPath(String original)
+    {
+        String numbers = "";
+        boolean isDigit = true;
+        for(int i = original.length() - 1; i >= 0 && isDigit; i--)
+        {
+            if(Character.isDigit(original.charAt(i)))
+                numbers += original.charAt(i);
+            else
+                isDigit = false;
+        }
+
+        if(numbers.length() == 0)
+            return original + "1";
+        else
+        {
+            int count = Integer.parseInt(numbers) + 1;
+            String sub = original.substring(0,sub.length() - numbers.length() + 1);
+            return sub + Integer.toString(count);
         }
     }
 }
