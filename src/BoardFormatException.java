9d848542d0449955cf87fa8e035b26a368dfe3a2
NapoleonBonerfarte <niklas.zwingenberger@gmail.com>
2014-10-24 00:01:06 +0100

    Completed.

--- src/BoardFormatException.java
+++ src/BoardFormatException.java
@@ -1,4 +1,11 @@
+//This exception is to be thrown by FileIO.java when a board is badly formatted.
 public class BoardFormatException extends Exception
 {
+	public BoardFormatException(){}
+
+	public BoardFormatException(String msg)
+	{
+		super(msg);
+	}
 
 }
