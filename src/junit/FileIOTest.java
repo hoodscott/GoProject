338e1083fc54c4e7788b22c5e9bf22dc67305a86
hoodscott <scottdothood@gmail.com>
2014-10-30 00:06:13 +0000

    Allowing the jar to use saves and info directories
    Modified  Ant build to copy the info and saveData directories to the
    same place the .jar is generated. This allows the jar to use the file
    paths that were already coded.

---
+++ src/junit/FileIOTest.java
@@ -0,0 +1,31 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class FileIOTest {
+
+	//private variables;
+	
+	@Before
+	public void setUp() throws Exception {
+		//instantiate object
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		//dereference object;
+	}
+
+	@Test
+	public void testSaveLog() {
+		assertTrue(false);
+	}
+	
+	@Test
+	public void testSaveBoard() {
+		assertTrue(false);
+	}
+
+}
---
+++ src/junit/TextUITest.java
@@ -0,0 +1,36 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TextUITest {
+
+	//private variable;
+	
+	@Before
+	public void setUp() throws Exception {
+		//instantiate object
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		//object=null;
+	}
+
+	@Test
+	public void testPathOS() {
+		assertTrue(false);
+	}
+	
+	@Test
+	public void testReadFile() {
+		assertTrue(false);
+	}
+	
+	@Test
+	public void testReadBoard() {
+		assertTrue(false);
+	}
+
+}
