f836fb951f8efcbe86bb36219c0dc1fdd5d6ded3
hoodscott <scottdothood@gmail.com>
2014-10-31 01:39:01 +0000

    Combining all TextUI junit tests into one file
    Also, implemented a few tests in FileIOTest.  Need to sort the file
    paths to finish implementing these tests.

--- src/junit/TextUITest.java
+++ src/junit/TextUITest.java
@@ -6,32 +6,168 @@ import org.junit.Test;
 
 public class TextUITest {
 
-	//private variables;
+	private GameEngine g;
+	private TextUI ti;
 	
 	@Before
 	public void setUp() throws Exception {
-		//instantiate objects
+		g = new GameEngine();
+		ti = new TextUI(g);
 	}
 
 	@After
 	public void tearDown() throws Exception {
-		//dereference objects;
+		g = null;
+		ti = null;
+	}
+	
+	/* NEW GAME METHOD TESTS */
+	
+	@Test
+	public void testNew9x9() {
+		String cmd[] = {"n"};
+		ti.newGame(cmd);
 	}
 	
 	@Test
+	public void testNewCustomBoard() {
+		String cmd1[] = {"n", "19", "19"};
+		ti.newGame(cmd1);
+		String cmd2[] = {"n","11","11"};
+		ti.newGame(cmd2);
+	}
+
+	@Test
+	public void testNewGameTooManyArgs() {
+		try {
+			String cmd[] = {"One","Two"};
+			ti.newGame(cmd);
+		} catch (Exception e) {
+			assertTrue(e instanceof BadInputException);
+		}
+	}
+	
+	@Test
+	public void testNewGameWrongDimensions() {
+		try {
+			String cmd[] = {"n","a","b"};
+			ti.newGame(cmd);
+		} catch (Exception e) {
+			assertTrue(e instanceof BadInputException);
+		}
+	}
+	
+	/* LOAD BOARD METHOD TESTS */
+	
+	@Test
+	public void testLoadBoard() {
+		String cmd1[] = {"Filepath"};
+		ti.loadBoard(cmd1);
+		String cmd2[] = {"lb","filepath"};
+		ti.loadBoard(cmd2);
+	}
+
+	@Test
+	public void testWrongArgsNum() {
+		try {
+			String cmd[] = {"1","2","3"};
+			ti.loadBoard(cmd);
+		} catch (Exception e) {
+			assertTrue(e instanceof BadInputException);
+		}
+	}
+	
+	/* SAVE LOG METHOD TESTS */
+	
+	@Test
 	//input interpreted correctly
+	public void testSaveLogInterpreted() {
+		String cmd[] = {"sl", "testLog"};
+		String log = "logged history";
+		ti.addToLog(log);
+		ti.saveLog(cmd);
+		// check FileIO.writeLog(log) is called
+		assertTrue(false);
+	}
+	
 	//exceptions thrown when input is incorrect
-	// '' 				when there is nothing to log
-	public void testSaveLog() {
-		assertTrue(false);
+	@Test
+	public void testSaveLogBadPath() {
+		try {
+			String cmd[] = {"sl","/badpath/testboard"};
+			String log = "logged history";
+			ti.addToLog(log);
+			ti.saveLog(cmd);
+		} catch (Exception e) {
+			assertTrue(e instanceof BadInputException);
+		}
 	}
+	@Test
+	public void testSaveLogTooManyArgs() {
+		try {
+			String cmd[] = {"sl","/badpath/testboard", "extraArg"};
+			String log = "logged history";
+			ti.addToLog(log);
+			ti.saveLog(cmd);
+		} catch (Exception e) {
+			assertTrue(e instanceof BadInputException);
+		}
+	}
+	
+	//exception thrown when log is empty
+	@Test
+	public void testSaveLogEmpty() {
+		try {
+			String cmd[] = {"sl"};
+			ti.saveLog(cmd);
+		} catch (Exception e) {
+			assertTrue(e instanceof BadInputException);
+		}
+	}
+	
+	/* SAVE BOARD METHOD TESTS */
 	
 	@Test
 	//input interpreted correctly
-	//exceptions are thrown when input is incorrect
-	// '' 					when nothing to save
-	public void testSaveBoard() {
+	public void testSaveBoardInterpreted() {
+		String cmd[] = {"sb", "testBoard"};
+		g.newGame();
+		ti.saveBoard(cmd);
+		// check FileIO.writeBoard(board) is called
 		assertTrue(false);
+	}
+	
+	//exceptions thrown when input is incorrect
+	@Test
+	public void testSaveBoardBadPath() {
+		try {
+			String cmd[] = {"sb","/badpath/testboard"};
+			g.newGame();
+			ti.saveBoard(cmd);
+		} catch (Exception e) {
+			assertTrue(e instanceof BadInputException);
+		}
+	}
+	@Test
+	public void testSaveBoardTooManyArgs() {
+		try {
+			String cmd[] = {"sb","/badpath/testboard", "extraArg"};
+			g.newGame();
+			ti.saveBoard(cmd);
+		} catch (Exception e) {
+			assertTrue(e instanceof BadInputException);
+		}
+	}
+	
+	//exception thrown when there is no board to save
+	@Test
+	public void testSaveBoardEmpty() {
+		try {
+			String cmd[] = {"sb"};
+			ti.saveBoard(cmd);
+		} catch (Exception e) {
+			assertTrue(e instanceof BadInputException);
+		}
 	}
 
 }
