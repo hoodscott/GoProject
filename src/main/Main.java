523ab3b8245a9b0847fcc3bef0b41f884ba53fa9
LausDaeus <niklas.zwingenberger@gmail.com>
2014-11-03 14:44:42 +0000

    test

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,8 +4,8 @@ public class Main{
 	//Use this class to test your stuff. Just comment out or delete your code before you commit it.
 	public static void main(String[] args)
 	{
-		GameEngine gE = new GameEngine();
-		TextUI tUI = new TextUI(gE);
-		tUI.init();
+            GameEngine gE = new GameEngine();
+            TextUI tUI = new TextUI(gE);
+            tUI.init();
 	}
 }
