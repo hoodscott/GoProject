1f62d31ad24932fa442d461527ecda0e1136f90c
LausDaeus <niklas.zwingenberger@gmail.com>
2014-11-21 11:15:03 +0000

    example board

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,16 +4,16 @@ public class Main{
 	//Use this class to test your stuff. Just comment out or delete your code before you commit it.
 	public static void main(String[] args)
 	{		
-			boolean gooey = false;
-			GameEngine gE = new GameEngine();
-			
-			if (gooey) {
-				GraphicalUI gUI = new GraphicalUI(gE);
-	            gUI.start(gE);
-			}
-			else {
-				TextUI tUI = new TextUI(gE);
-				tUI.init();
+            boolean gooey = false;
+            GameEngine gE = new GameEngine();
+
+            if (gooey) {
+                GraphicalUI gUI = new GraphicalUI(gE);
+                gUI.start(gE);
+            }
+            else {
+                    TextUI tUI = new TextUI(gE);
+                    tUI.init();
             }
 	}
 }
