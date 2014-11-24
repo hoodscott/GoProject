36d51e23df4740cc97af1633256cab7278e1689e
Kiril <kirilhristov91@gmail.com>
2014-11-24 21:05:09 +0000

    Objective changes

--- src/main/Objective.java
+++ src/main/Objective.java
@@ -13,6 +13,7 @@ public class Objective {
     // Action white;
     Action action;
     int colour;
+    int otherColour;
     Coordinate position;
     
     //Objective Constructor, the text should adhere the appropriate format, containing the colour this objective is for.
@@ -29,6 +30,12 @@ public class Objective {
     			return true;
     	}
     	
+    	if (action.equals(Action.KILL)){
+    		if(board.get(position.getX(), position.getY()) == getOtherColour()) 
+    			return true;
+    	} 
+    	
+    	/*
     	// check if there are 2 eyes
     	if (action.equals(Action.KILL)){
     		// TODO
@@ -36,17 +43,23 @@ public class Objective {
     	
     	// also if both players pass and KILL objective is not accomplished => failed?
     	// or just if both players pass => false (defend) and true (kill)
-    	
+    	*/
     	return false;
     }
     
     //Checks if the objective succeeded for the given player.
     public boolean checkSucceeded(Board board){
-    	if (action.equals(Action.KILL)){
-    		if(board.get(position.getX(), position.getY()) == 0) 
+    	if (action.equals(Action.DEFEND)){
+    		if(board.get(position.getX(), position.getY()) == colour) 
     			return true;
     	}
     	
+    	if (action.equals(Action.KILL)){
+    		if(board.get(position.getX(), position.getY()) != getOtherColour()) 
+    			return true;
+    	}
+    	
+    	/*
     	// check if there are 2 eyes
     	if (action.equals(Action.DEFEND)){
     		// TODO
@@ -54,8 +67,14 @@ public class Objective {
     	}
     	// also if both players pass and KILL objective is not accomplished => failed?
     	// or just if both players pass => false (defend) and true (kill)
+    	*/
     	
     	return false;
+    }
+    
+    public int getOtherColour(){
+    	if(colour == 1) return 2;
+    	return 1;
     }
     
     //Returns whether the player plays first or not.
