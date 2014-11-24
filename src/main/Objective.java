b50d732393f8c265b892187bad8f15b69826506c
LausDaeus <niklas.zwingenberger@gmail.com>
2014-11-24 11:25:51 +0000

    Simplifying of objectives

--- src/main/Objective.java
+++ src/main/Objective.java
@@ -3,8 +3,16 @@ package main;
 //Holds objective for a player and assesses whether it is met or not.
 public class Objective {
     
+    enum Action{
+        KILL;
+        DEFEND;
+    }
+    
+    Action black;
+    Action white;
+    
     //Objective Constructor, the text should adhere the appropriate format, containing the colour this objective is for.
-    public Objective(String text, int colour){}
+    public Objective(String text){}
     
     //Checks if the objective failed for the given player.
     public boolean checkFailed(){return false;}
