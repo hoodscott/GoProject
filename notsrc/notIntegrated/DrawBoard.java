4e7035a2541e6b1053628a7cc880a0541c0209d2
eilidhbagel <fleureilidh@hotmail.com>
2014-11-27 17:29:45 +0000

    Draw counters on grid test

--- notsrc/notIntegrated/DrawBoard.java
+++ notsrc/notIntegrated/DrawBoard.java
@@ -23,13 +23,41 @@ public class DrawBoard extends Frame {
   
 	// Draw board
 	public void paint(Graphics g) {
-		// draw board of 9 lines 50 pixels in from sides
+		// Draw board of specified lines
 		int lines = 9;
-		int squareSize = 550/9;
+		int frameSize = 600;
+		int squareSize = (frameSize)/lines;
+		int stoneSize = squareSize/2;
 			
-		for ( int i = 0; i < lines; i++) {
-			g.drawLine(i*squareSize+squareSize,squareSize,i*squareSize+squareSize,squareSize*lines);
-			g.drawLine(squareSize,i*squareSize+squareSize,squareSize*lines,i*squareSize+squareSize);
+		for(int x=0;x<lines;x++) {
+		    for(int y=0;y<lines;y++) {
+		        g.drawRect(x*squareSize,y*squareSize,squareSize,squareSize);
+		    }
+		}
+		
+		int[][] exampleBoard = {{0,0,0,1,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,2,0,0,0},
+				{0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0},
+				{0,1,0,0,0,0,0,0,0},
+				{0,0,0,0,0,0,0,0,0},
+				{0,0,0,0,0,2,1,0,0},
+				{0,2,0,0,0,0,0,0,0}};
+		
+		// Draws counters on grid
+		for (int i = 0; i<lines; i++) {
+			for (int j = 0; j<lines; j++) {
+				if (exampleBoard[i][j] == 1) {
+					g.setColor(Color.black);
+					g.fillOval(j*squareSize-stoneSize,i*squareSize-stoneSize,squareSize,squareSize);
+				}
+				else if (exampleBoard[i][j] == 2) {
+					g.drawOval(j*squareSize-stoneSize,i*squareSize-stoneSize,squareSize,squareSize);
+					g.setColor(Color.white);
+					g.fillOval(j*squareSize-stoneSize,i*squareSize-stoneSize,squareSize,squareSize);
+				}
+			}
 		}
 	}
 	
