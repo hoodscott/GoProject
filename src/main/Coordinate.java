0ec9c5ee93ed43c9d803dcf584ddddb801e1c9f6
LausDaeus <niklas.zwingenberger@gmail.com>
2014-11-27 17:55:30 +0000

    Removed excess methods.

--- src/main/Coordinate.java
+++ src/main/Coordinate.java
@@ -8,20 +8,4 @@ public class Coordinate{
             this.x = x;
             this.y = y;
     }
-
-    public void setX(int x){
-    	this.x = x;
-    }
-
-    public void setY(int y){
-    	this.x = y;
-    }
-    
-    public int getX(){
-    	return x;
-    }
-    
-    public int getY(){
-    	return y;
-    }
 }
