be08a5ce12d3f1c9237474cb82b205a7cc584bb1
eilidhbagel <fleureilidh@hotmail.com>
2014-11-30 15:23:44 +0000

    Mouse events place stones on board

--- src/main/BoardJPanel.java
+++ src/main/BoardJPanel.java
@@ -8,7 +8,16 @@ import javax.swing.JPanel;
 public class BoardJPanel extends JPanel {
 
 	// constants
-	public static final int BOARD_LENGTH = 610;
+	public static final int BOARD_LENGTH = 600;
+	public int[][] exampleBoard = { { 0, 0, 0, 1, 0, 0, 0, 0, 0}, 
+			{ 0, 0, 0, 1, 0, 0, 0, 0, 0}, 
+			{ 0, 0, 0, 0, 0, 0, 2, 0, 0}, 
+			{ 0, 0, 0, 1, 2, 0, 0, 0, 0}, 
+			{ 0, 0, 0, 0, 0, 2, 0, 0, 0}, 
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
+			{ 0, 0, 0, 0, 0, 2, 0, 0, 0}, 
+			{ 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
 	// Board constructor
 	public BoardJPanel() {
@@ -22,21 +31,32 @@ public class BoardJPanel extends JPanel {
 			public void mouseClicked(MouseEvent e) {
 				double x = e.getPoint().getX();
 				double y = e.getPoint().getY();
-				int squareSize = 590 / (8); // TODO: make global variable
-				updateBoard((int)x/squareSize,(int)y/squareSize);
+				int squareSize = 600 / (8); // TODO: make global variable
+				
+				// Get position of counter
+				int xPos = (int)x/squareSize;
+				int xRemainder = (int)x-(xPos*squareSize);
+				if (xRemainder > squareSize/2) xPos++;
+				int yPos = (int)y/squareSize;
+				int yRemainder = (int)y-(yPos*squareSize);
+				if (yRemainder > squareSize/2) yPos++;
+				
+				// Update board with co-ordinates and stone colour
+				updateBoard(xPos,yPos,1); // SET TO BLACK STONE
 			}
 		});
 	}
 	
-	public void updateBoard(int x, int y) {
-		System.out.println(x);
-		System.out.println(y);
+	// Draw counter onto position
+	public void updateBoard(int x, int y, int colour) {
+		exampleBoard[y][x] = colour;
+		repaint();
 	}
 
 	// Draw board
 	public void paint(Graphics g) {
 		int lines = 9; // test int
-		int frameSize = BOARD_LENGTH-10; // allow 10 pixel space for int division
+		int frameSize = BOARD_LENGTH;
 		int squareSize = (frameSize) / (lines-1);
 		int stoneSize = squareSize / 2;
 
@@ -52,16 +72,6 @@ public class BoardJPanel extends JPanel {
 						squareSize);
 			}
 		}
-
-		int[][] exampleBoard = { { 0, 0, 0, 1, 0, 0, 0, 0, 0}, 
-				{ 0, 0, 0, 1, 0, 0, 0, 0, 0}, 
-				{ 0, 0, 0, 0, 0, 0, 2, 0, 0}, 
-				{ 0, 0, 0, 1, 2, 0, 0, 0, 0}, 
-				{ 0, 0, 0, 0, 0, 2, 0, 0, 0}, 
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0}, 
-				{ 0, 0, 0, 0, 0, 2, 0, 0, 0}, 
-				{ 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
 		// Draws counters on grid
 		for (int i = 0; i < lines; i++) {
