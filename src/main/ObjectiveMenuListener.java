3f7e379678dc2966e8dc202145ce3bd339b2dac8
hoodscott <scottdothood@gmail.com>
2015-01-07 23:43:47 +0000

    Objective now updates with objective file menu
    When objective is either updated or removed, the objective label is
    updated. I may have used a lazy try/catch block  but we will need to
    sanity check the objective input at some point.

--- src/main/ObjectiveMenuListener.java
+++ src/main/ObjectiveMenuListener.java
@@ -13,31 +13,33 @@ public class ObjectiveMenuListener implements ActionListener {
 		// allow user to set objective
 		if (e.getActionCommand().equals("Set Objective")) {
 			Component frame = null;
-			String objective = (String) JOptionPane
-					.showInputDialog(
-							frame,
-							"Specify Objective (Action, Colour, Stone Position):",
-							"Set Bounds", JOptionPane.PLAIN_MESSAGE, null,
-							null, "");
+			String objective = (String) JOptionPane.showInputDialog(frame,
+					"Specify Objective (Action, Colour, Stone Position):",
+					"Set Bounds", JOptionPane.PLAIN_MESSAGE, null, null, "");
 			if (objective != null) {
 				// TODO: Check for valid input
-				String[] objParts = objective.split(" ");
-				Coordinate pos = new Coordinate(
-						Integer.parseInt(objParts[2]),
-						Integer.parseInt(objParts[3]));
-				Objective obj = new Objective(objParts[0],
-						Integer.parseInt(objParts[1]), pos);
-				BoardJPanel.setObjective(obj);
-				GraphicalUI.feedback.setText("Objective updated");
+				try {
+					String[] objParts = objective.split(" ");
+					Coordinate pos = new Coordinate(
+							Integer.parseInt(objParts[2]),
+							Integer.parseInt(objParts[3]));
+					Objective obj = new Objective(objParts[0],
+							Integer.parseInt(objParts[1]), pos);
+					BoardJPanel.setObjective(obj);
+					GraphicalUI.feedback.setText("Objective updated");
+					GraphicalUI.objective.setText(GraphicalUI.getGameEngine()
+							.getObjective().toString());
+				} catch (Exception exception) {
+					// TODO fix this bad thing ~someone~ did
+					// (involves checking objective input is in correct form)
+					GraphicalUI.feedback.setText("Invalid objective supplied");
+				}
 			} else {
-				GraphicalUI.feedback.setText("Invalid objective supplied");
+				// remove objective from board and label
+				BoardJPanel.setObjective(null);
+				GraphicalUI.feedback.setText("Objective removed");
+				GraphicalUI.objective.setText("None specified");
 			}
-		} else {
-			// remove objective from board and label
-			BoardJPanel.setObjective(null);
-			GraphicalUI.feedback.setText("Objective removed");
-			GraphicalUI.objective.setText("None Specified");
 		}
 	}
-
 }
