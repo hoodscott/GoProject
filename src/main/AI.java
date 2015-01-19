ed714c8157fdc61322b102038f3a7e0b1bfeff05
LausDaeus <niklas.zwingenberger@gmail.com>
2015-01-19 22:43:14 +0000

    Documentation for AI.

--- src/main/AI.java
+++ src/main/AI.java
@@ -1,5 +1,6 @@
 package main;
 
+//This is the abstract class for all AI types to extend.
 public abstract class AI {
     
     //These bounds define the space that should be searched by the AI on the board. It is ensured beforehand, that they are within the board.
@@ -8,10 +9,13 @@ public abstract class AI {
     protected int lowerBoundY;
     protected int upperBoundY;
     
+    //Colour of the AI
     protected int colour;
     
+    //NextMove method to be implemented by subclasses
     public abstract Coordinate nextMove(Board b, LegalMoveChecker lMC);
     
+    //Sets the bounds for the AI to search in. Notably, the input should be in the (x1, y1, x2, y2) format.
     public void setBounds(int[] data){
         if(data[0] > data[2]){
             upperBoundX = data[0];
