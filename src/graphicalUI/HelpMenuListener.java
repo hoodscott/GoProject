aa152cd96163baf0c682452ba8114bfbc0efbb71
hoodscott <scottdothood@gmail.com>
2015-01-30 12:52:34 +0000

    Adding method to view the keyboard shortcuts
    Still need to implement a better way to display the commands, it just
    prints them to the console. Maybe a popup window with the commands
    nicely formatted.

--- src/graphicalUI/HelpMenuListener.java
+++ src/graphicalUI/HelpMenuListener.java
@@ -2,14 +2,24 @@ package graphicalUI;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import main.FileIO;
 
 public class HelpMenuListener implements ActionListener {
 
-    // TODO implement helper actions
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getActionCommand().equals("Shout for Help")) {
-            // show user help
-        }
-    }
+	// TODO implement helper actions
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO fix this
+		if (e.getActionCommand().equals("Shout for Help")) {
+			String filePath = FileIO.pathOS(FileIO.RELATIVEPATH + "\\info\\GUIShortcuts");
+			ArrayList<String> keyboardFile = FileIO
+					.readFile(filePath);
+			// TODO display shortcuts in a new popup to the user
+			for (String t : keyboardFile) {
+				System.out.println(t);
+			}
+		}
+	}
 }
