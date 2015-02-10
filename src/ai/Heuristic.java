616594c5755af1e9f84a6668819ac553aeca05ab
Kiril <kirilhristov91@gmail.com>
2015-02-10 13:23:30 +0000

    Heuristic interface minor change

--- src/ai/Heuristic.java
+++ src/ai/Heuristic.java
@@ -6,5 +6,5 @@ public interface Heuristic {
     
     //General heuristic method to implement and use by alpha-beta.
     //When the search space is adapted, will be added.
-    public int assess(Board b, LegalMoveChecker lmc, Objective obj);
+    public int assess(Board b, LegalMoveChecker lmc, Objective obj, int colourAI);
 }
