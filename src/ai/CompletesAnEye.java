de51a7556b23cdbff182caaf11c1ebd2204c9fac
jamie00170 <jamiedd13@aol.com>
2015-02-17 14:04:04 +0000

    small change to CompletesAnEye.java

--- src/ai/CompletesAnEye.java
+++ src/ai/CompletesAnEye.java
@@ -10,8 +10,6 @@ import ai.Rating;
 // return appropriate value
 public class CompletesAnEye implements Heuristic{
 	
-	
-	
 	@Override
 	/// gives a board a move, obj and player to move 
 	// should return COMPLETES_AN_EYE if a move
@@ -23,9 +21,11 @@ public class CompletesAnEye implements Heuristic{
 		// variable for determining how many out of 4 stones are in place
 		// to create an eye (x+1,y),(x-1,y),(x,y-1) and (x,y+1)
 		int eye_counter = 0;
+		boolean isAlmostEye = false;
 		byte[][] board_to_assess = b.getRaw();
 		//iterate over the board 
 		for (int x = 0; x < x_limit;x++){
+			eye_counter = 0;
 			for (int y = 0; y < y_limit; y++){
 				
 				if(board_to_assess[x+1][y] == colourAI )
@@ -36,21 +36,16 @@ public class CompletesAnEye implements Heuristic{
 					eye_counter++;
 				if(board_to_assess[x][y-1] == colourAI )
 					eye_counter++;
+				if (eye_counter == 3)
+					isAlmostEye = true;
 			}
 		}
 			
-		if (eye_counter == 3){
+		if (isAlmostEye){
 		return Rating.COMPLETES_AN_EYE.getValue();
 		}else{
 			return 0;
 		}
 	}
-	
-	
-	
-	
-	//TODO method for detecting eyes
-	
-	
 	
 }
