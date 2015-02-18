b2aad22e652daeabe71541e3c3c41a8720a1f96c
jamie00170 <jamiedd13@aol.com>
2015-02-18 16:40:55 +0000

    SevenStonesInARow.java now checks in the corners for five stones in a row

--- src/ai/SevenStonesInARow.java
+++ src/ai/SevenStonesInARow.java
@@ -21,6 +21,7 @@ public class SevenStonesInARow implements Heuristic{
 		
 		int stone_counter = 0;
 		boolean isSevenInARow = false;
+		boolean isFiveInARow = false;
 		byte[][] board_to_assess = b.getRaw();
 		//iterate over the board 
 		// only search edge rows + columns [1,y], [x,1], [width -1,y] and [x, height-1]
@@ -72,11 +73,119 @@ public class SevenStonesInARow implements Heuristic{
 					isSevenInARow = true;
 			}
 			
-		if (isSevenInARow){
+		
+		
+		
+		// search for 5 stones in the corners
+		
+		// search [1,y] corners
+					
+					for (int y = 0; y <= 5; y++){
+						
+						if(board_to_assess[1][y] == colourAI ){
+							stone_counter++;
+						}else{
+							stone_counter = 0;
+						}
+						if (stone_counter == 5)
+							isFiveInARow = true;
+					}
+					
+					stone_counter = 0;
+					for (int y = 13; y <= 17; y++){
+						
+						if(board_to_assess[1][y] == colourAI ){
+							stone_counter++;
+						}else{
+							stone_counter = 0;
+						}
+						if (stone_counter == 5)
+							isFiveInARow = true;
+					}
+					
+		// search [x,1] corners
+					stone_counter = 0;
+					for (int x = 0; x <= 5; x++){
+									
+							if(board_to_assess[x][1] == colourAI ){
+									stone_counter++;
+							}else{
+									stone_counter = 0;
+							}
+							if (stone_counter == 5)
+								isFiveInARow = true;
+								}
+								
+					stone_counter = 0;
+					for (int x = 13; x <= 17; x++){
+									
+							if(board_to_assess[x][1] == colourAI ){
+									stone_counter++;
+							}else{
+									stone_counter = 0;
+							}
+							if (stone_counter == 5)
+								isFiveInARow = true;
+								}
+					
+		// search [width -1 ,y] corners
+					stone_counter = 0;
+					for (int y = 0; y <= 5; y++){
+									
+							if(board_to_assess[x_limit - 1][y] == colourAI ){
+									stone_counter++;
+							}else{
+									stone_counter = 0;
+							}
+							if (stone_counter == 5)
+								isFiveInARow = true;
+								}
+								
+					stone_counter = 0;
+					for (int y = 13; y <= 17; y++){
+									
+							if(board_to_assess[x_limit - 1][y] == colourAI ){
+									stone_counter++;
+							}else{
+									stone_counter = 0;
+							}
+							if (stone_counter == 5)
+								isFiveInARow = true;
+								}
+					
+		// search [x ,height -1] corners
+					stone_counter = 0;
+					for (int x = 0; x <= 5; x++){
+									
+							if(board_to_assess[x][y_limit - 1] == colourAI ){
+									stone_counter++;
+							}else{
+									stone_counter = 0;
+							}
+							if (stone_counter == 5)
+								isFiveInARow = true;
+								}
+								
+					stone_counter = 0;
+					for (int x = 13; x <= 17; x++){
+									
+							if(board_to_assess[x][y_limit -1] == colourAI ){
+									stone_counter++;
+							}else{
+									stone_counter = 0;
+							}
+							if (stone_counter == 5)
+								isFiveInARow = true;
+								}
+				
+					
+	if (isFiveInARow)
+		return Rating.MAKES_SIX_IN_A_ROW.getValue();
+	if(isSevenInARow){
 		return Rating.MAKES_EIGHT_IN_A_ROW.getValue();
-		}else{
-			return 0;
-		}
+	}else{
+		return 0;
 	}
 	
+	}
 }
