2d78d4d2f7d64e85542e13280b8bc7d5e3129383
jamie00170 <jamiedd13@aol.com>
2015-02-20 09:24:15 +0000

    Updating CompletesAnEye.java to search edges for eyes, still update to check corners

--- src/ai/CompletesAnEye.java
+++ src/ai/CompletesAnEye.java
@@ -21,12 +21,20 @@ public class CompletesAnEye implements Heuristic{
 		// variable for determining how many out of 4 stones are in place
 		// to create an eye (x+1,y),(x-1,y),(x,y-1) and (x,y+1)
 		int eye_counter = 0;
+		int edgeEye_counter = 0;
+		int cornerEye_counter = 0;
+		
 		boolean isAlmostEye = false;
+		boolean isAlmostEdgeEye = false;
+		boolean isAlmostCornerEye = false;
 		byte[][] board_to_assess = b.getRaw();
-		//iterate over the board 
-		for (int x = 0; x < x_limit;x++){
+;
+		
+		
+		//iterate over the board excluding the edges
+		for (int x = 1; x < (x_limit - 2);x++){
 			eye_counter = 0;
-			for (int y = 0; y < y_limit; y++){
+			for (int y = 1; y < (y_limit - 2); y++){
 				
 				if(board_to_assess[x+1][y] == colourAI )
 					eye_counter++;
@@ -40,7 +48,82 @@ public class CompletesAnEye implements Heuristic{
 					isAlmostEye = true;
 			}
 		}
+		
+		//check edges i.e. [0,y], [x,0], [x_limit - 1,y], [x,y_limit -1]
+		// omitting corner points
+		
+		//search [0,y]
+		edgeEye_counter = 0;
+		for (int y = 1; y < (y_limit - 2); y++){
 			
+			if(board_to_assess[0][y-1] == colourAI )
+				edgeEye_counter++;
+			if(board_to_assess[1][y] == colourAI )
+				edgeEye_counter++;
+			if(board_to_assess[0][y+1] == colourAI ){
+				edgeEye_counter++;
+			}else{
+				edgeEye_counter = 0;
+			}
+			if (edgeEye_counter == 2)
+				isAlmostEdgeEye = true;
+		}
+		
+		// search [x,0]
+		edgeEye_counter = 0;
+		for (int x = 1; x < (x_limit - 2); x++){
+			
+			if(board_to_assess[x-1][0] == colourAI )
+				edgeEye_counter++;
+			if(board_to_assess[x][1] == colourAI )
+				edgeEye_counter++;
+			if(board_to_assess[x+1][0] == colourAI ){
+				edgeEye_counter++;
+			}else{
+				edgeEye_counter = 0;
+			}
+			if (edgeEye_counter == 2)
+				isAlmostEdgeEye = true;
+		}
+		
+		//search [x,y_limit-1]
+		edgeEye_counter = 0;
+		for (int x = 1; x < (x_limit - 2); x++){
+			
+			if(board_to_assess[x-1][y_limit-1] == colourAI )
+				edgeEye_counter++;
+			if(board_to_assess[x][y_limit-2] == colourAI )
+				edgeEye_counter++;
+			if(board_to_assess[x+1][y_limit-1] == colourAI ){
+				edgeEye_counter++;
+			}else{
+				edgeEye_counter = 0;
+			}
+			if (edgeEye_counter == 2)
+				isAlmostEdgeEye = true;
+		}
+		
+		//search [x_limit -1,y]
+				edgeEye_counter = 0;
+				for (int y = 1; y < (y_limit - 2); y++){
+					
+					if(board_to_assess[x_limit-1][y-1] == colourAI )
+						edgeEye_counter++;
+					if(board_to_assess[x_limit-2][y] == colourAI )
+						edgeEye_counter++;
+					if(board_to_assess[x_limit-1][y+1] == colourAI ){
+						edgeEye_counter++;
+					}else{
+						edgeEye_counter = 0;
+					}
+					if (edgeEye_counter == 2)
+						isAlmostEdgeEye = true;
+				}
+				
+		// now check the 4 corner points
+		// check [0,0]
+		
+		
 		if (isAlmostEye){
 		return Rating.COMPLETES_AN_EYE.getValue();
 		}else{
