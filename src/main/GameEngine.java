73cdc6d12153a3b143ce4d2a24762e1f10f0b928
LausDaeus <niklas.zwingenberger@gmail.com>
2015-03-10 09:59:21 +0000

    Removed old print statements

--- src/main/GameEngine.java
+++ src/main/GameEngine.java
@@ -51,15 +51,7 @@ public class GameEngine {
     }
 
     // Returns the current board to the caller 
-    public Board getCurrentBoard() {
-        byte[][] values = currentBoard.getRaw();
-        for(int i = 0; i < values[0].length; i++){
-            for(int j = 0; j < values.length; j++)
-                System.out.print(" "+values[j][i]);
-            System.out.println();
-        }
-        System.out.println();
-                
+    public Board getCurrentBoard() {                
         return currentBoard;
     }
 
