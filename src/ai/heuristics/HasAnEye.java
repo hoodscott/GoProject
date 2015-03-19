41ad4976736ccd47eb0d5b6d60fc0c8103c4b65a
jamie00170 <jamiedd13@aol.com>
2015-03-19 10:29:32 +0000

    adding images and also change to my heurstic

--- src/ai/heuristics/HasAnEye.java
+++ src/ai/heuristics/HasAnEye.java
@@ -19,10 +19,10 @@ public class HasAnEye implements Heuristic{
 	/// gives a board a move, obj and player to move 
 	// should return COMPLETES_AN_EYE if a move
 	// is found to return an eye else 0
-	public int assess(Board initialBoard, Board b, LegalMoveChecker lmc, Objective obj, int colourAI) {
+	public int assess(Board initialBoard, Board currentBoard, LegalMoveChecker lmc, Objective obj, int colourAI) {
 		
-		int x_limit = b.getWidth();
-		int y_limit = b.getHeight();
+		int x_limit = initialBoard.getWidth();
+		int y_limit = initialBoard.getHeight();
 		// variable for determining how many out of 4 stones are in place
 		// to create an eye (x+1,y),(x-1,y),(x,y-1) and (x,y+1)
 		int eye_counter = 0;
@@ -42,7 +42,7 @@ public class HasAnEye implements Heuristic{
 		
 		//iterate over the board excluding the edges
 		for (int x = 1; x <= (x_limit - 2);x++){
-			eye_counter = 0;
+			//eye_counter = 0;
 			for (int y = 1; y <= (y_limit - 2); y++){
 				
 				if(initialBoard.get( x+1, y) == colourAI )
@@ -54,10 +54,12 @@ public class HasAnEye implements Heuristic{
 				if(initialBoard.get(x, y-1) == colourAI )
 					eye_counter++;
 				
-				if (eye_counter == 4)
+				
+				
+				if (eye_counter == 4 && currentBoard.get(x,y) == Board.EMPTY_AI){
 					isEye = true;
 				    eye_counter = 0;
-				    twoEye_counter++;
+				    twoEye_counter++;}
 				
 			}
 		}
@@ -78,9 +80,10 @@ public class HasAnEye implements Heuristic{
 			}else{
 				edgeEye_counter = 0;
 			}
-			if (edgeEye_counter == 3)
+			if (edgeEye_counter == 3 && currentBoard.get(0,y) == Board.EMPTY_AI)
 				isEdgeEye = true;
 			    twoEye_counter++;
+			    
 		}
 		
 		// search [x,0]
@@ -96,10 +99,10 @@ public class HasAnEye implements Heuristic{
 			}else{
 				edgeEye_counter = 0;
 			}
-			if (edgeEye_counter == 3)
+			if (edgeEye_counter == 3 && currentBoard.get(x,0) == Board.EMPTY_AI)
 				isEdgeEye = true;
 				twoEye_counter++;
-		}
+		}		
 		
 		//search [x,y_limit-1]
 		edgeEye_counter = 0;
@@ -114,9 +117,10 @@ public class HasAnEye implements Heuristic{
 			}else{
 				edgeEye_counter = 0;
 			}
-			if (edgeEye_counter == 3)
+			if (edgeEye_counter == 3 && currentBoard.get(x, y_limit) == Board.EMPTY_AI)
 				isEdgeEye = true;
 				twoEye_counter++;
+				
 		}
 		
 		//search [x_limit -1,y]
@@ -132,23 +136,25 @@ public class HasAnEye implements Heuristic{
 					}else{
 						edgeEye_counter = 0;
 					}
-					if (edgeEye_counter == 3)
+					if (edgeEye_counter == 3 && currentBoard.get(x_limit ,y) == Board.EMPTY_AI)
 						isEdgeEye = true;
 						twoEye_counter++;
+						
 				}
 				
 		// now check the 4 corner points
 		
-		if (initialBoard.get(0,1) == colourAI && initialBoard.get(1,0) == colourAI ){
+		if (initialBoard.get(0,1) == colourAI && initialBoard.get(1,0) == colourAI && currentBoard.get(0,0) == Board.EMPTY_AI ){
+			isCornerEye = true;
+			
+		}
+		if (initialBoard.get(0, y_limit-2) == colourAI && initialBoard.get(1, y_limit-1) == colourAI && currentBoard.get(0, y_limit-1) == Board.EMPTY_AI ){
 			isCornerEye = true;
 		}
-		if (initialBoard.get(0, y_limit-2) == colourAI && initialBoard.get(1, y_limit-1) == colourAI ){
+		if (initialBoard.get(x_limit-2, 0) == colourAI && initialBoard.get(x_limit-1,1) == colourAI && currentBoard.get(x_limit -1, 0) == Board.EMPTY_AI){
 			isCornerEye = true;
 		}
-		if (initialBoard.get(x_limit-2, 0) == colourAI && initialBoard.get(x_limit-1,1) == colourAI ){
-			isCornerEye = true;
-		}
-		if (initialBoard.get(x_limit-1, y_limit-2) == colourAI && initialBoard.get(x_limit-2, y_limit-1) == colourAI ){
+		if (initialBoard.get(x_limit-1, y_limit-2) == colourAI && initialBoard.get(x_limit-2, y_limit-1) == colourAI && currentBoard.get(x_limit - 1,y_limit -1) == Board.EMPTY_AI){
 			isCornerEye = true;
 		}
 		
@@ -173,10 +179,10 @@ public class HasAnEye implements Heuristic{
 				if(initialBoard.get(x1+1, y1) == colourAI )
 					eye_counter++;
 						
-				if (eye_counter == 6)
+				if (eye_counter == 6 && initialBoard.get(x,y) == Board.EMPTY_AI &&  initialBoard.get(x1,y1) == Board.EMPTY_AI){
 					is2pointEye = true;
 				    eye_counter = 0;
-				    twoEye_counter++;
+				    twoEye_counter++;}
 						
 			}
 		}
@@ -186,7 +192,7 @@ public class HasAnEye implements Heuristic{
 				
 				//search [0,y]
 				edgeEye_counter = 0;
-				for (int y = 1, y1 = 2; y1 <= (y_limit - 2); y++){
+				for (int y = 1, y1 = 2; y1 <= (y_limit - 2); y++, y1++){
 					
 					if(initialBoard.get(0,y-1) == colourAI )
 						edgeEye_counter++;
@@ -203,13 +209,14 @@ public class HasAnEye implements Heuristic{
 					if (edgeEye_counter == 4){
 						is2pointEdgeEye = true;
 					    twoEye_counter++;
+					    edgeEye_counter=0;
 				    }			
 				
 				}
 				
 				// search [x,0]
 				edgeEye_counter = 0;
-				for (int x = 1, x1 = 2; x1 <= (x_limit - 2); x++){
+				for (int x = 1, x1 = 2; x1 <= (x_limit - 2); x++, x1++){
 					
 					if(initialBoard.get(x-1,0) == colourAI )
 						edgeEye_counter++;
@@ -225,51 +232,56 @@ public class HasAnEye implements Heuristic{
 					if (edgeEye_counter == 4)
 						is2pointEdgeEye = true;
 						twoEye_counter++;
+						edgeEye_counter=0;
 					}
 				}	
 				
 			    //search [x,y_limit-1]
 				edgeEye_counter = 0;
-				for (int x = 1, x1 = 2; x <= (x_limit - 2); x++){
+				for (int x = 1, x1 = 2; x1 <= (x_limit - 2); x++, x1++){
 						
 					if(initialBoard.get(x-1, y_limit-1) == colourAI )
 						edgeEye_counter++;
 					if(initialBoard.get(x, y_limit-2) == colourAI )
 						edgeEye_counter++;
-					if(initialBoard.get(x1, y_limit-2) == colourAI ){
+					if(initialBoard.get(x1, y_limit-2) == colourAI )
 						edgeEye_counter++;
 					if(initialBoard.get(x1+1, y_limit-1) == colourAI ){
 						edgeEye_counter++;
 					}else{
 						edgeEye_counter = 0;
 					}
-					if (edgeEye_counter == 4)
+					if (edgeEye_counter == 4){
 						is2pointEdgeEye = true;
 						twoEye_counter++;
+						edgeEye_counter=0;
 					}
 				}
 					
 				//search [x_limit -1,y]
 				edgeEye_counter = 0;
-				for (int y = 1, y1 = 2; y <= (y_limit - 2); y++){
+				for (int y = 1, y1 = 2; y1 <= (y_limit - 2); y++, y1++){
 					
 					if(initialBoard.get(x_limit-1,y-1) == colourAI )
 						edgeEye_counter++;
 					if(initialBoard.get(x_limit-2,y) == colourAI )
 						edgeEye_counter++;
-					if(initialBoard.get(x_limit-2,y1) == colourAI ){
+					if(initialBoard.get(x_limit-2,y1) == colourAI )
 						edgeEye_counter++;
 					if(initialBoard.get(x_limit-1,y1+1) == colourAI ){
 						edgeEye_counter++;
 					}else{
 						edgeEye_counter = 0;
 					}
-					if (edgeEye_counter == 4)
+					if (edgeEye_counter == 4){
 						is2pointEdgeEye = true;
 						twoEye_counter++;
+						edgeEye_counter=0;
 					}	
 				}
 				
+		
+		
 				
 		if (twoEye_counter >= 2){
 			return Rating.HAS_A_SECOND_EYE.getValue();
@@ -284,8 +296,8 @@ public class HasAnEye implements Heuristic{
 		}
 		
 		}
-		
+	}
 		
 	}
 	
-}
+	
